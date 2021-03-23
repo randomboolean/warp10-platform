5ab5b946c9b09e9b66e3e1608a34fdfb72a52a5b
Jean-Charles <jean-charles.vialatte@senx.io>
2021-03-23 18:20:16 +0100

    Update WarpTest.java

--- warp10/src/test/java/io/warp10/WarpTest.java
+++ warp10/src/test/java/io/warp10/WarpTest.java
@@ -68,9 +68,9 @@ public class WarpTest {
     FileWriter fr = new FileWriter(new File(extraConfPath));
     fr.write("warp.timeunits = us\n");
 
-    fr.write("warpscript.extension.ml = ml.MachineLearningPackage\n");
-    fr.write("ml.package.sandbox = sandbox:11T3mi3HaWV5E7u3vzlOhp8Tqt7yeKfcHcryu2MNqEYY\n");
-    fr.write("warpscript.defaultcl.ml.MachineLearningPackage = true\n");
+    //fr.write("warpscript.extension.ml = ml.MachineLearningPackage\n");
+    //fr.write("");
+    //fr.write("warpscript.defaultcl.ml.MachineLearningPackage = true\n");
     fr.close();
     conf.add(extraConfPath);
 
